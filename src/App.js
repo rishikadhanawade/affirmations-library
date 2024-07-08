6b6abce67551d18fb5e163a2c74fc4e099fda651
Rishika Dhanawade <rishika.dhanawade@gmail.com>
2024-07-08 20:07:34 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [affirmations, setAffirmations] = useState({});
+
+  useEffect(() => {
+    fetch('/affirmations.json')
+      .then(response => response.json())
+      .then(data => setAffirmations(data));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Affirmations</h1>
+        {Object.keys(affirmations).map(category => (
+          <div key={category}>
+            <h2>{category.replace(/-/g, ' ')}</h2>
+            <ul>
+              {affirmations[category].map((affirmation, index) => (
+                <li key={index}>{affirmation}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </header>
     </div>
   );
